2089aaf8c329a07f54f85f9dbf589636faf572c8
Khabizat Sharapudinova <habizat22@gmail.com>
2022-06-08 14:21:02 +0000

    Update

--- db/seeds/05_messages.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO messages (buyer_id, shoe_id, message, is_admin)
-VALUES
-(1, 4, 'Hi, do they run small? And what is the heel height? Thanks', false),
-(2, 1, 'Hi, do they run true to size? And what is the heel height? Thanks', false);
