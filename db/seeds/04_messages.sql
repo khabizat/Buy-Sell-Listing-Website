a24b5df57d468ffbbdbff5f4557a234668a07809
Khabizat Sharapudinova <habizat22@gmail.com>
2022-06-10 02:19:02 +0000

    DB reset

--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -1,4 +1,4 @@
-INSERT INTO messages (buyer_id, shoe_id, message, is_admin)
+INSERT INTO messages (buyer_id, shoe_id, message)
 VALUES
-(1, 4, 'Hi, do they run small? And what is the heel height? Thanks', false),
-(2, 1, 'Hi, do they run true to size? And what is the heel height? Thanks', false);
+(1, 4, 'Hi, do they run small? And what is the heel height? Thanks'),
+(2, 1, 'Hi, do they run true to size? And what is the heel height? Thanks');
