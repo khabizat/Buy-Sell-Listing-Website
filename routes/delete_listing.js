7b728eb9e7c0322f510d254ffb2a53ddfd9add74
Khabizat Sharapudinova <habizat22@gmail.com>
2022-06-10 15:44:56 +0000

    Initial commit

---
+++ routes/delete_listing.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router  = express.Router();
+
+
+const deleteListing = function(seller_id, db) {
+  return db
+  .query(`
+  DELETE FROM shoes
+  WHERE seller_id = $1
+  AND shoes.id = 20;
+  `, [seller_id])
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+}
+
+module.exports = (db) => {
+  router.post('/', (req, res) => {
+    // const shoe_id = 9;
+    const user_id = req.session.user_id;
+    deleteListing(user_id, db)
+    .then(result => {
+      res.redirect("listings");
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e)
+    })
+  })
+
+return router;
+};
